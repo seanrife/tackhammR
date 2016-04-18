2d7febe6c6344bd07d6c2c1bf413e2817b85e7c2
Sean C. Rife <seanrife@gmail.com>
2016-04-18 10:37:48 -0500

    Initial commit

---
+++ tackhamm.R
@@ -0,0 +1,12 @@
+# tackhammR by Sean C. Rife // @seanrife // seanrife.com // srife1@murraystate.edu
+# Version 0.01 (alpha)
+# To be run prior to any analyses
+
+tackhammR <- function(){
+  x <- readline("Are you going to run any formal tests of a null hypothesis?")  
+  
+  if(x=="yes"){
+    print("Please locate a tack hammer and bludgeon yourself with it.")
+  }
+  
+}
